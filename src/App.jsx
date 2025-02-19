cc39a6ff5e06e02b3eb771e870debc7a070420d2
jutdelu <jtdelunardo@gmail.com>
2025-02-19 20:21:01 -0300

    fix: refatorando app

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,24 @@ import diceService from "./services/diceService";
 import DiceButton from "./components/DiceButton";
 import imageDice from "./assets/dice.svg";
 import "./App.css";
-import { grey } from "@mui/material/colors";
+
+const DiceButtonContainer = ({ buttons }) => (
+	<Box display="flex" justifyContent="center" alignItems="center" minHeight="25vh">
+		<Card sx={{ padding: 1 }}>
+			<CardContent>
+				<Box display="flex" justifyContent="center" gap={2}>
+					{buttons.map((button, index) => (
+						<DiceButton key={index} label={button.label} onClick={button.onClick} />
+					))}
+				</Box>
+			</CardContent>
+		</Card>
+	</Box>
+);
 
 function App() {
 	const [diceRoll, setDiceRoll] = useState(null);
+	const [diceType, setDiceType] = useState("");
 
 	const rollDice = async (faces) => {
 		const result = await diceService.rollDice(faces);
@@ -18,82 +32,28 @@ function App() {
 		setDiceType(faces);
 	};
 
-	const DiceButtonContainer = ({ buttons }) => (
-		<Box
-			display="flex"
-			justifyContent="center"
-			alignItems="center"
-			minHeight="25vh"
-		>
-			<Card sx={{ padding: 1 }}>
-				<CardContent>
-					<Box
-						display="flex"
-						justifyContent="center"
-						gap={2}
-					>
-						{buttons.map((button, index) => (
-							<DiceButton
-								key={index}
-								label={button.label}
-								onClick={button.onClick}
-							/>
-						))}
-					</Box>
-				</CardContent>
-			</Card>
-		</Box>
-	);
-
 	const buttons = [
 		{ label: "D4", onClick: () => rollDice(4) },
 		{ label: "D6", onClick: () => rollDice(6) },
 		{ label: "D8", onClick: () => rollDice(8) },
 	];
 
-	const [diceType, setDiceType] = useState("");
-
 	return (
-		<Box
-		sx={{
-			display: "flex",
-			justifyContent: "center",
-			alignItems: "center",
-			height: "100vh", // Para garantir que ocupe toda a altura da tela
-			padding: 2, // Um pouco de padding para não ficar grudado nas bordas
-		}}
-	>
-		<Card 
-			sx={{ maxWidth: 700, margin: "auto", padding: 0, textAlign: "center" }}
-		>
-			<CardContent  sx={{ padding: "12px" }}>
-				<Header title="Sorteio de Dados!" />
-				<div>
-					<a className="container">
-						<img
-							src={imageDice}
-							className="logo"
-							alt="Dice logo"
-						/>
-					</a>
-				</div>
-				<div
-					style={{
-						display: "flex",
-						flexDirection: "column",
-						alignItems: "center",
-						gap: "2px",
-					}}
-				>
-					<DiceButtonContainer buttons={buttons} />
-					<DiceResult
-						result={diceRoll}
-						diceType={diceType}
-					/>
-				</div>
-				<Footer text="feito por jutdelu" />
-			</CardContent>
-		</Card></Box>
+		<Box display="flex" justifyContent="center" alignItems="center" height="100vh" padding={2}>
+			<Card sx={{ maxWidth: 700, margin: "auto", padding: 0, textAlign: "center" }}>
+				<CardContent sx={{ padding: "12px" }}>
+					<Header title="Sorteio de Dados!" />
+					<Box className="container" sx={{ minHeight: "150px" }}>
+						<img src={imageDice} className="logo" alt="Dice logo" />
+					</Box>
+					<Box display="flex" flexDirection="column" alignItems="center" gap={2}>
+						<DiceButtonContainer buttons={buttons} />
+						<DiceResult result={diceRoll} diceType={diceType} />
+					</Box>
+					<Footer text="feito por jutdelu" />
+				</CardContent>
+			</Card>
+		</Box>
 	);
 }
 
